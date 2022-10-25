78778fb9be0ddbf67fc2584e8a0ded86c2684b61
Иван Механиков <ivanmehanikov@MacBook-Air-Ivan.local>
2022-10-25 18:47:06 +0600

    Создал компоненту SortPopup

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -13,8 +13,10 @@ const Categories = ({items}) => {
         (
             <div className="categories">
             <ul>
-              <li>Все</li>
-              {items.map((item, index) => <li 
+              <li onClick={() => onActiveItem(null)}
+              className={activeItem === null ? 'active' : ''}
+              >Все</li>
+              {items && items.map((item, index) => <li 
               onClick={() => onActiveItem(index)}
               className={activeItem === index ? 'active' : ''}
               key={`${item}_${index}`}>{item}</li>)}
