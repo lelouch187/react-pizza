221257e3fc918aa1484a8b5905a9844dc81c3111
lelouch187 <vanushs187@gmail.com>
2023-01-15 17:23:50 +0600

    add pizzas.json and categories map

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 const Categories = () => {
+  const [isActive,setIsActive] = React.useState(0)
+  const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые']
+  const onSetIsActive = (index) => {
+    setIsActive(index)
+  }
+
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((item,index)=>{
+          return <li key={item}
+          onClick={()=>onSetIsActive(index)} 
+          className={isActive===index?'active':''}>{item}</li>
+        })}
       </ul>
     </div>
   );
 };
 
-export default Categories
+export default Categories;
